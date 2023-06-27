06d1ef2095ae0c475fd96bb2dd5c15cb7dfd3f0b
Rafael Delboni <rafadelboni@gmail.com>
2023-06-26 22:17:13 -0300

    feat: adds notes table and some fixes

--- resources/migrations/20230615131142-add-inital-tables.down.sql
+++ resources/migrations/20230615131142-add-inital-tables.down.sql
@@ -1,47 +1,21 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+DROP TABLE IF EXISTS notes;
 
 --;;
 
-CREATE TABLE IF NOT EXISTS authors (
-  author_id uuid UNIQUE NOT NULL PRIMARY KEY,
-  login VARCHAR(150) NOT NULL,
-  account_source VARCHAR(150) NOT NULL,
-  avatar_url VARCHAR(500) NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-  UNIQUE (login, account_source)
-);
+DROP TABLE IF EXISTS examples_authors;
 
 --;;
 
-CREATE TABLE IF NOT EXISTS see_alsos (
-  see_also_id uuid UNIQUE NOT NULL PRIMARY KEY,
-  author_id uuid NOT NULL,
-  definition_id VARCHAR(500) NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk_author_see_also
-    FOREIGN KEY(author_id)
-      REFERENCES authors(author_id)
-);
+DROP TABLE IF EXISTS examples;
 
 --;;
 
-CREATE TABLE IF NOT EXISTS examples (
-  example_id uuid UNIQUE NOT NULL PRIMARY KEY,
-  author_id uuid NOT NULL,
-  definition_id VARCHAR(500) NOT NULL,
-  body TEXT NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP WITH TIME ZONE,
-  CONSTRAINT fk_author_see_also
-    FOREIGN KEY(author_id)
-      REFERENCES authors(author_id)
-);
+DROP TABLE IF EXISTS see_alsos;
 
 --;;
 
-CREATE TABLE examples_editors (
-  example_id int REFERENCES examples(example_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  author_id int REFERENCES authors(author_id) ON UPDATE CASCADE,
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT examples_editors_pkey PRIMARY KEY (example_id, author_id)
-);
+DROP TABLE IF EXISTS authors;
+
+--;;
+
+DROP EXTENSION IF EXISTS "uuid-ossp";
--- resources/migrations/20230615131142-add-inital-tables.up.sql
+++ resources/migrations/20230615131142-add-inital-tables.up.sql
@@ -1,17 +1,54 @@
-DROP TABLE IF EXISTS examples_editors;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 --;;
 
-DROP TABLE IF EXISTS examples;
+CREATE TABLE IF NOT EXISTS authors (
+  author_id uuid UNIQUE NOT NULL PRIMARY KEY,
+  login VARCHAR(150) NOT NULL,
+  account_source VARCHAR(150) NOT NULL,
+  avatar_url VARCHAR(500) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE (login, account_source)
+);
 
 --;;
 
-DROP TABLE IF EXISTS see_alsos;
+CREATE TABLE IF NOT EXISTS see_alsos (
+  see_also_id uuid UNIQUE NOT NULL PRIMARY KEY,
+  author_id uuid NOT NULL,
+  definition_id VARCHAR(500) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT author_see_also_fk FOREIGN KEY(author_id) REFERENCES authors(author_id)
+);
 
 --;;
 
-DROP TABLE IF EXISTS authors;
+CREATE TABLE IF NOT EXISTS examples (
+  example_id uuid UNIQUE NOT NULL PRIMARY KEY,
+  definition_id VARCHAR(500) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP WITH TIME ZONE
+);
 
 --;;
 
-DROP EXTENSION IF EXISTS "uuid-ossp";
+CREATE TABLE examples_authors (
+  example_id uuid REFERENCES examples(example_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  author_id uuid REFERENCES authors(author_id) ON UPDATE CASCADE,
+  body TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT example_author_fk FOREIGN KEY(author_id) REFERENCES authors(author_id),
+  CONSTRAINT example_body_fk FOREIGN KEY(example_id) REFERENCES examples(example_id)
+);
+
+--;;
+
+CREATE TABLE IF NOT EXISTS notes (
+  note_id uuid UNIQUE NOT NULL PRIMARY KEY,
+  author_id uuid NOT NULL,
+  definition_id VARCHAR(500) NOT NULL,
+  body TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP WITH TIME ZONE,
+  CONSTRAINT author_note_fk FOREIGN KEY(author_id) REFERENCES authors(author_id)
+);
