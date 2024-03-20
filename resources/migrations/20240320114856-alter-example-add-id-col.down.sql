9ea2ff40e2b8a59db86d50888a8a1fc40e57d8d3
Rafael Delboni <rafadelboni@gmail.com>
2024-03-20 10:36:07 -0300

    fix: adds id to example-edit-table

---
+++ resources/migrations/20240320114856-alter-example-add-id-col.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE example_edit DROP CONSTRAINT example_edit_pkey;
+
+--;;
+
+ALTER TABLE example_edit DROP COLUMN example_edit_id;
---
+++ resources/migrations/20240320114856-alter-example-add-id-col.up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE example_edit ADD COLUMN example_edit_id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid ();
