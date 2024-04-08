6596c50bc0127f3f8fb84cc4bf6c9a32d8edfc7d
Karina Moreira <karina.moreira@nubank.com.br>
2024-04-08 15:45:43 -0300

    Aero reader for csv

--- src/codes/clj/docs/backend/config.clj
+++ src/codes/clj/docs/backend/config.clj
@@ -1,28 +1,20 @@
 (ns codes.clj.docs.backend.config
-  (:require [clojure.string :as string]))
-
-(def vector-env-vars
-  "Nested keys for config variables with corresponding environment 
-  variables that can hold multiple values separated by commas, e.g.:
-  ALLOWED_ORIGINS=\"https://domain.a.com, https://domain.b.com\""
-  [[:config :webserver/allowed-origins]])
+    (:require [aero.core :as aero]
+              [clojure.string :as string]))
 
 (defn- str-var->vector-var
   "Converts a string config variable to a vector of strings, when applicable.
   Environment variables are expected to be set as comma-separated values."
-  [config nested-keys]
-  (let [target-config (get-in config nested-keys)]
-    (if (string? target-config)
-      (let [split-configs (-> target-config
+  [value]
+  (if (string? value)
+      (let [split-configs (-> value
                               (string/split  #","))
             env-config (->> split-configs
                             (map string/trim)
                             (remove empty?))]
-        (assoc-in config nested-keys env-config))
-      config)))
+        env-config)
+      value))
 
-(defn config
-  ([config]
-   (reduce str-var->vector-var config vector-env-vars))
-  ([config vector-envs]
-   (reduce str-var->vector-var config vector-envs)))
+(defmethod aero/reader 'csv 
+  [_ _ value]
+  (str-var->vector-var value))
